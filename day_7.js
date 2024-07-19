ded9fdb26fb552a638ad46bdd5c5fc22421b5755
Vishwajeet Kumar <vishwajeetkumarpatelmgs@gmail.com>
2024-07-19 09:35:23 +0530

    Day 7 [Objects] of 30 days of JavaScript Completed

--- day_7.js
+++ day_7.js
@@ -8,7 +8,6 @@ console.log(book.title);
 console.log(book.author);
 console.log(book.year);
 
-
 // Task 2: Access and log the title and author properties of the book object.
 console.log(book["title"]);
 console.log(book["author"]);
